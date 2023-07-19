90278a1430ee4b73fe87f39849a698f6b0fded5f
Galaxomas <thefidanabdulla@gmail.com>
2023-07-19 16:03:07 +0400

    run and jump animation code is done

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,31 +20,36 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInp = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalInp * speed, rb.velocity.y);
 
-        if (horizontalInp > 0.01)
+        if (horizontalInp > 0.01f)
         {
             transform.localScale = new Vector3(10, 10, 1);
-            //anim.SetBool("isWalking", true);
+            
 
         }        
-        else if (horizontalInp < -0.01)
+        else if (horizontalInp < -0.01f)
         {
             transform.localScale = new Vector3(-10,10,1);
-            //anim.SetBool("isWalking", true);
-        }
-        else
-        {
-            //anim.SetBool("isWalking", false);
+            
         }
 
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && grounded)
         {
             Jump();
         }
+        anim.SetBool("run", horizontalInp != 0);
+        anim.SetBool("grounded", grounded);
     }
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, speed);
+        anim.SetTrigger("jump");
         grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") 
+            grounded = true;
     }
 
 }
